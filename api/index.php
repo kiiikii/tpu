3b880fd7af8f805c003c1e1486c65303c93cb620
kiiikii <30403399+kiiikii@users.noreply.github.com>
2025-12-26 10:46:28 +0700

    Update file includes to use __DIR__ constant

--- api/index.php
+++ api/index.php
@@ -1,15 +1,15 @@
 <?php 
 	
-	include '/../_loader.php';
+	include __DIR__ '/_loader.php';
 	if (isset($_GET['halaman'])) {
 		$halaman = $_GET['halaman'];
 	} else {
 		$halaman = 'beranda';
 	}
 	ob_start();
-	$file = '/../_page/'.$halaman.'.php';
+	$file = __DIR__ '../_page/'.$halaman.'.php';
 	if (!file_exists($file)) {
-		include '/../_page/404.php';
+		include __DIR__  '/_page/404.php';
 	} else {
 		include $file;
 	}
@@ -19,20 +19,20 @@
 
 <!DOCTYPE html>
 <html lang="en">
-	<?php include '/../_layout/head.php' ?>
+	<?php include __DIR__ '/_layout/head.php' ?>
 	<body>
-		<?php include '/../_layout/nav.php' ?>
+		<?php include __DIR__ '/_layout/nav.php' ?>
 		<?php 
 			echo $halaman;
-			include '/../_layout/footer.php'
+			include __DIR__ '/_layout/footer.php'
 		?>
-		<?php include '/../_layout/loader.php' ?>
-		<?php include '/../_layout/javascript.php' ?>
+		<?php include __DIR__ '/_layout/loader.php' ?>
+		<?php include __DIR__ '/_layout/javascript.php' ?>
 	</body>
 </html>
 
 <?php 
 	if (isset($jsfile)) {
-		include '/../_page/js/'.$jsfile.'.php';
+		include __DIR__ '/_page/js/'.$jsfile.'.php';
 	}
 ?>
